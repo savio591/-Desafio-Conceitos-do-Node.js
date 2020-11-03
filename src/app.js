4892691398da7ecddb45448561ea54e178b94672
savio591 <savio591@hotmail.com>
2020-11-03 17:26:38 -0300

    Doing GET and POST from GoStack challenge

--- src/app.js
+++ src/app.js
@@ -1,26 +1,58 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+// Criador da lista de repositórios
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+
+  // Retornar apenas a variável "repositories" enquanto o filtro não estiver preparado.
+  return response.json(repositories)
 });
 
+// Criador de repositórios.
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, link, techs } = request.body;
+  const likes = 0
+
+  const repository = { id: uuid(), title, link, techs, likes }
+
+  repositories.push(repository)
+
+  return response.json(repositories);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, link, techs } = request.body;
+
+  const repoIndex = repositories.findIndex((repository) => repository.id == id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: "Project not found" });
+  }
+
+  const repository = {
+    id,
+    title,
+    link,
+    techs,
+    likes
+  };
+  repositories[repoIndex] = repository;
+  
+  return response.json(repositories)
+
 });
+
+
 
 app.delete("/repositories/:id", (request, response) => {
   // TODO
